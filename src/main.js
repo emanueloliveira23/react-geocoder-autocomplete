ad1c79be9ced3d1ade6983dad3a3e7551e81e356
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2018-05-15 14:57:06 -0300

    add propt-types; remove react prop-types

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var React = require('react'),
   ReactDOM = require('react-dom'),
   FlipMove = require('react-flip-move'),
+  PropTypes = require('prop-types'),
   search = require('./search');
 
 /**
@@ -40,24 +41,24 @@ var Geocoder = React.createClass({
     };
   },
   propTypes: {
-    endpoint: React.PropTypes.string,
-    defaultInputValue: React.PropTypes.string,
-    source: React.PropTypes.string,
-    inputClass: React.PropTypes.string,
-    resultClass: React.PropTypes.string,
-    resultsClass: React.PropTypes.string,
-    inputPosition: React.PropTypes.string,
-    inputPlaceholder: React.PropTypes.string,
-    resultFocusClass: React.PropTypes.string,
-    onSelect: React.PropTypes.func.isRequired,
-    onSuggest: React.PropTypes.func,
-    onInputChange: React.PropTypes.func,
-    accessToken: React.PropTypes.string.isRequired,
-    proximity: React.PropTypes.string,
-    bbox: React.PropTypes.string,
-    showLoader: React.PropTypes.bool,
-    focusOnMount: React.PropTypes.bool,
-    types: React.PropTypes.string
+    endpoint: PropTypes.string,
+    defaultInputValue: PropTypes.string,
+    source: PropTypes.string,
+    inputClass: PropTypes.string,
+    resultClass: PropTypes.string,
+    resultsClass: PropTypes.string,
+    inputPosition: PropTypes.string,
+    inputPlaceholder: PropTypes.string,
+    resultFocusClass: PropTypes.string,
+    onSelect: PropTypes.func.isRequired,
+    onSuggest: PropTypes.func,
+    onInputChange: PropTypes.func,
+    accessToken: PropTypes.string.isRequired,
+    proximity: PropTypes.string,
+    bbox: PropTypes.string,
+    showLoader: PropTypes.bool,
+    focusOnMount: PropTypes.bool,
+    types: PropTypes.string
   },
   componentWillMount() {
     this.setState({inputValue: this.props.defaultInputValue});
